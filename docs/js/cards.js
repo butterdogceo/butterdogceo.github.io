2c8e8bf5ea89c75b9b5d9215b741b883b4f065d2
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2024-09-06 11:39:46 -0700

    Update scripts and added additional card features.

---
+++ docs/js/cards.js
@@ -0,0 +1,31 @@
+const cards = {
+  ["home"]:[
+    {
+      Name:"buterdogco",
+      Desc:"da best company on earht",
+      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog.png",
+      Link:"https://butterdogceo.github.io/bdogco/"
+    },
+    {
+      Name:"wikabedia",
+      Desc:"uplode epik artikles instentley!",
+      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/icons/Wikabedia.png",
+      Link:"https://butterdogceo.github.io/Wikabedia/"
+    },
+    {
+      Name:"da hub",
+      Desc:"mom can i play",
+      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/icons/da%20hub.png",
+      Link:"https://joebidenrealomg.github.io/da-hub/",
+      Secret:true
+    },
+    {
+      Name:"butterdog chat",
+      Desc:"yap yap",
+      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog%20chat.png",
+      Link:"https://bdogco.natbees.cl",
+      Secret:true,
+      Disabled:true
+    }
+  ],
+};
