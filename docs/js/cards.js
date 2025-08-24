65214adccb7b3958f423f934ae18024a19badc4d
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2025-08-25 04:22:26 +0800

    Removed unnecessary card data

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -1,106 +1,4 @@
 const cards = {
-  ["home"]:[
-    {
-      Name:"learn more with Wikabedia",
-      Icon:"img/icons/Wikabedia.png",
-      Link:"https://wikabedia.b-dog.co",
-      AltLink:"https://butterdogceo.github.io/Wikabedia/",
-    },
-
-    {
-      Name:"post some garbage on bweeter",
-      Icon:"img/icons/Bweeter.png",
-      Link:"https://bweeter.bettermode.io/",
-    },
-    
-    {
-      Name:"check out some more apps",
-      Icon:"img/icons/Apps.png",
-      Link:"applications",
-    }
-  ],
-  ["apps"]:[
-    {
-      Name:"boogle",
-      Desc:"da best serch engun",
-      Icon:"img/icons/Boogle.png",
-      Link:"https://boogle.b-dog.co"
-    },
-    {
-      Name:"wikabedia",
-      Desc:"wikabedia uplode epik artikles instentley!",
-      Icon:"img/icons/Wikabedia.png",
-      Link:"https://wikabedia.b-dog.co",
-      AltLink:"https://butterdogceo.github.io/Wikabedia/"
-    },
-    {
-      Name:"butterFlix",
-      Desc:"super 99.99% real epik movie",
-      Icon:"img/icons/ButterFlix.png",
-      Link:"https://sites.google.com/view/butter-flix/"
-    },
-    {
-      Name:"bulu",
-      Desc:"hours of ads wiht 30 secund movie brake",
-      Icon:"img/icons/Bulu.png",
-      Link:"https://sites.google.com/view/bulu/"
-    },
-    {
-      Name:"butter+",
-      Desc:"movie but wiht de plus",
-      Icon:"img/icons/ButterPlus.png",
-      Link:"https://sites.google.com/view/butterplus"
-    },
-    {
-      Name:"butterfiy",
-      Desc:"want brake from ad? more ad",
-      Icon:"img/icons/Butterfy.png",
-      Link:"https://sites.google.com/bsd48.org/buterfiy/"
-    },
-    {
-      Name:"LEADERBOARD",
-      Desc:"BSD CODE OF CONDUCT VIOLATION LEADERBOARD 🦅🇺🇲",
-      Icon:"img/catipillar.jpg",
-      Link:"https://butterdogceo.github.io/CODE-OF-CONDUCT-VIOLATION-LEADERBOARD/"
-    },
-    {
-      Name:"butter webstore",
-      Desc:"get teh new buter for ur computehr",
-      Icon:"img/icons/Webstore.png",
-      Link:"https://sites.google.com/view/butter-dog-search/apps/bdog-webstore/"
-    },
-    {
-      Name:"bweeter",
-      Desc:"most epic socal meda (beta)",
-      Icon:"img/icons/Bweeter.png",
-      Link:"https://bweeter.bettermode.io"
-    },
-    {
-      Name:"bbay",
-      Desc:"pay money for te thing you pay for",
-      Icon:"img/icons/Bbay.png",
-      Link:"https://sites.google.com/bsd48.org/bbay"
-    },
-    {
-      Name:"butterOS",
-      Desc:"da latest and greatest ting for ur computer",
-      Icon:"img/icons/ButterOS.png",
-      Link:"https://b-dog.co/ButterOS/",
-      AltLink: "https://butterdogceo.github.io/ButterOS/"
-    },
-    {
-      Name:"butterdonalds",
-      Desc:"cheapest food for healthiest option 👍",
-      Icon:"img/icons/ButterDonalds.png",
-      Link:"https://sites.google.com/bsd48.org/bdonalds"
-    },
-    {
-      Name:"calculater",
-      Desc:"realy smart computor taht do math",
-      Icon:"img/icons/Calculator.jpeg",
-      Link:"tools/calculator"
-    }
-  ],
   ["althub"]:[
     {
       Name:"buterdogco",
@@ -116,7 +14,7 @@ const cards = {
     },
     {
       Name:"violation",
-      Desc:"BSD CODE OF CONDUCT VIOLATION LEADERBOARD 🦅🇺🇲",
+      Desc:"BSD CODE OF CONDUCT VIOLATION LEADERBOARD 🦅🇺🇸",
       Icon:"https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/catipillar.jpg?raw=true",
       Link:"https://butterdogceo.github.io/CODE-OF-CONDUCT-VIOLATION-LEADERBOARD/",
       Secret:true
