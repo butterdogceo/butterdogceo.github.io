6abb1f3abbb99e4c43f14aa490e9807260de5078
ButterDogCEO <100174775+butterdogceo@users.noreply.github.com>
2025-12-07 22:56:59 -0800

    Fixed missing resources, removed unused resources, improved code formatting, and removed outdated cards

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -1,58 +1,43 @@
 const cards = {
-  ["althub"]:[
+  ["althub"]: [
     {
-      Name:"buterdogco",
-      Desc:"da best company on earht",
-      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog.png",
-      Link:"https://butterdogceo.github.io/bdogco/"
+      Name: "buterdogco",
+      Desc: "da best company on earht",
+      Icon: "https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog.png",
+      Link: "https://butterdogceo.github.io/bdogco/"
     },
     {
-      Name:"wikabedia",
-      Desc:"uplode epik artikles instentley!",
-      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/icons/Wikabedia.png",
-      Link:"https://butterdogceo.github.io/Wikabedia/"
+      Name: "violation tracker",
+      Desc: "BSD CODE OF CONDUCT VIOLATION TRACKER 🦅🇺🇸",
+      Icon: "https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/catipillar.jpg?raw=true",
+      Link: "https://butterdogceo.github.io/CODE-OF-CONDUCT-VIOLATION-LEADERBOARD/"
     },
     {
-      Name:"violation",
-      Desc:"BSD CODE OF CONDUCT VIOLATION LEADERBOARD 🦅🇺🇸",
-      Icon:"https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/catipillar.jpg?raw=true",
-      Link:"https://butterdogceo.github.io/CODE-OF-CONDUCT-VIOLATION-LEADERBOARD/",
-      Secret:true
+      Name: "butterOS",
+      Desc: "da latest and greatest ting for ur computer, with da hub integration (testing)",
+      Icon: "https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/icons/ButterOS.png",
+      Link: "https://butterdogceo.github.io/ButterOS/"
     },
     {
-      Name:"da hub",
-      Desc:"mom can i play",
-      Icon:"https://raw.githubusercontent.com/joebidenrealomg/da-hub/main/img/Logo%20Transparent.png?raw=true",
-      Link:"https://joebidenrealomg.github.io/da-hub/",
-      Secret:true
+      Name: "eagler",
+      Desc: "dang minceraft download real",
+      Icon: "https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/eagler.jpeg?raw=true",
+      Link: "https://drive.usercontent.google.com/u/0/uc?id=1Oo4XahrELEzxBX6ckopfUIW02qWsamH_&export=download",
+      Secret: true
     },
     {
-      Name:"eagler",
-      Desc:"dang minceraft download real",
-      Icon:"https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/eagler.jpeg?raw=true",
-      Link:"https://drive.usercontent.google.com/u/0/uc?id=1Oo4XahrELEzxBX6ckopfUIW02qWsamH_&export=download",
-      Secret:true
+      Name: "youtube kids",
+      Desc: "da BEST video platform!!",
+      Icon: "https://github.com/butterdogceo/youtube-kids/blob/main/docs/img/youtube%20kids%20square.png?raw=true",
+      Link: "https://butterdogceo.github.io/youtube-kids/",
+      Secret: true
     },
     {
-      Name:"youtube kids",
-      Desc:"da BEST video platform!!",
-      Icon:"https://github.com/butterdogceo/youtube-kids/blob/main/docs/img/youtube%20kids%20square.png?raw=true",
-      Link:"https://butterdogceo.github.io/youtube-kids/",
-      Secret:true
-    },
-    {
-      Name:"butterdog chat",
-      Desc:"yap yap (PUBLIC TESTING)",
-      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog%20chat.png",
-      Link:"https://butterdogceo.github.io/about.html",
-      Secret:true,
-    },
-    {
-      Name:"butterOS",
-      Desc:"da latest and greatest ting for ur computer, with da hub integration (testing)",
-      Icon:"https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/icons/ButterOS.png",
-      Link:"https://butterdogceo.github.io/ButterOS/",
-      Secret:true
+      Name: "butterdog chat",
+      Desc: "yap yap (PUBLIC TESTING)",
+      Icon: "https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog%20chat.png",
+      Link: "https://butterdogceo.github.io/about.html",
+      Secret: true,
     }
   ],
 };
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,6 @@
 const cardsContainer = document.getElementById("cards");
 const nav = document.getElementById("nav");
+const icon = document.getElementById("icon");
 
 // Card creation function
 // cardInfo attributes: Name, Desc?, Icon, Link, AltLink?, Secret?, Disabled?
@@ -25,7 +26,7 @@ function createCard(cardInfo) {
 
     // We don't want to apply this if a link is a page of this repo
     // But we should still allow links that include butterdogco.com
-    if (!(a.href.includes("://butterdogco.com") && a.href.split("/").length <= 4)) {
+    if (!(a.href.includes("://butterdogceo.github.io") && a.href.split("/").length <= 4)) {
       // Open in a new tab
       a.target = "_blank";
       a.title += " in a new tab";
@@ -73,45 +74,6 @@ try {
   console.error(err);
 }
 
-// DVD logo funny bouncing images
-const funnyEnabled = true;
-const funnyImages = [
-  "https://w7.pngwing.com/pngs/626/579/png-transparent-blu-ray-disc-computer-icons-dvd-compact-disc-dvd-text-logo-desktop-wallpaper-thumbnail.png",
-  "https://github.com/butterdogco/butterdogco.github.io/blob/main/docs/img/catipillar.jpg?raw=true",
-  "https://upload.wikimedia.org/wikipedia/en/8/85/Bill_Nye_the_Science_Guy_title_screen.jpg",
-  "https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/woah%20cat.jpg?raw=true",
-  "https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/main/docs/img/butterdog.png?raw=true"
-];
-
-// Utility function that gets a random number between the 2 ranges (no way really)
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-}
-
-// Creates the bouncing images and sets up an update loop for them
-function createFunny() {
-  funnyImages.forEach(function (image) {
-    const img = document.createElement("img");
-    img.classList.add("dvd");
-    img.src = image;
-    document.body.appendChild(img);
-
-    function updatePosition() {
-      const randomX = getRandomInt(-50, screen.availWidth);
-      const randomY = getRandomInt(-50, screen.availHeight);
-      const randomSomething = Math.random() * 500;
-      img.style.left = `${randomX}px`;
-      img.style.top = `${randomY}px`;
-      img.style.width = `${randomSomething}px`;
-      img.style.transform = `rotateX(${randomSomething}deg) rotateY(${randomSomething}deg) rotateZ(${randomSomething}deg)`;
-      setTimeout(updatePosition, getRandomInt(10, 1000));
-    }
-    updatePosition();
-  });
-}
-
 // Take a guess as to what this does
 function toggleMenu() {
   if (nav) {
@@ -121,61 +83,24 @@ function toggleMenu() {
 
 // Plays the funny sound
 function playFunnySound() {
-  const audio = new Audio("audio/this%20look%20like%20gaming%20area.mp3");
+  const audio = new Audio("https://raw.githubusercontent.com/butterdogco/butterdogco.github.io/refs/heads/main/docs/audio/this%20look%20like%20gaming%20area.mp3");
   audio.controls = true;
   document.head.appendChild(audio);
   audio.play();
 }
 
-let rotateNum = 0;
+let iconRotation = 0;
 let musicStarted = false;
 
-// I wonder when this runs
 function iconClick() {
   playFunnySound();
-  createFunny();
   if (musicStarted === false) {
     musicStarted = true;
     const audio = new Audio("audio/dubstep.wav");
     document.head.appendChild(audio);
     audio.play();
   }
-  rotateNum = rotateNum + 180;
-  document.getElementById('icon').style.rotate = `${rotateNum}deg`;
+  iconRotation += 180;
+  icon.style.rotate = `${rotateNum}deg`;
   document.body.classList.add("secret");
-}
-
-// Alternate Site Detection
-// ⚠ Modifying these functions violates ButterDogCo Terms of Service ⚠
-const link = window.location.href;
-if (!link.includes("https://butterdogco.com")) {
-  const item = document.createElement('a');
-  const div = document.getElementById('nav');
-  if (div) {
-    item.innerText = "Important Info";
-    item.href = "altsite";
-    div.appendChild(item);
-
-    if (link.includes("/altsite")) {
-      item.href = "#";
-      item.id = "active";
-      const p = document.getElementById('altSiteInfoText');
-      if (link.includes("https://butterdogceo.github.io") && p) {
-        p.innerHTML = "This is an alternate website for ButterDogCo, the main website can be found <a href='https://butterdogco.com'>here</a>.";
-      } else if (p) {
-        p.innerHTML = "This is an unofficial website for ButterDogCo. Please note that anything found here can be modified by the site host, the official website can be found <a href='https://butterdogco.com'>here</a>.";
-      }
-    }
-  }
-
-  var things = document.querySelectorAll("a");
-  var things_array = [...things];
-  things_array.forEach(obj => {
-    if (obj.getAttribute('data-alt-link')) {
-      obj.href = obj.getAttribute('data-alt-link');
-      obj.title = "Click to open alternate link";
-    }
-  });
-} else if (link.includes("/altsite")) {
-  window.location.href = "index";
 }
